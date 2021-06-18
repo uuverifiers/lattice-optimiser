e5410bd928f6f2d232ce8d009f8a666f35ee47d0
Philipp Ruemmer <ph_r@gmx.net>
2021-06-18 17:25:22 +0200

    test cases exposing a previous bug

--- src/test/scala/lattopt/DependentProductLatticeTests.scala
+++ src/test/scala/lattopt/DependentProductLatticeTests.scala
@@ -1,16 +1,18 @@
 
 package lattopt;
 
+import scala.collection.immutable.BitSet
+
 import org.scalacheck.Properties
 
 object DependentProductLatticeTests extends Properties("DependentProductLattice") {
 
-  property("Simple DependentProductLattice") = {
     val lattice1 =
       for (bv1 <- BitSetLattice(4); bv2 <- BitSetLattice(6);
            if !bv1(0) || !bv2(0))
       yield (bv1, bv2, bv1.size + bv2.size)
 
+  property("Simple DependentProductLattice") = {
     lattice1.getLabel(lattice1.top)._3 == 10 &&
     lattice1.isFeasible(lattice1.bottom) &&
     !lattice1.isFeasible(lattice1.top) && {
@@ -35,6 +37,16 @@ object DependentProductLatticeTests extends Properties("DependentProductLattice"
         )
       }
     }
+  }
+
+  property("incomparables") = {
+    val lb = (BitSet(), BitSet(0)).asInstanceOf[lattice1.LatticeObject]
+    val comp = (BitSet(1, 2, 3),
+                BitSet(0, 1, 2, 3, 4, 5)).asInstanceOf[lattice1.LatticeObject]
+
+    val incomps = lattice1.incomparableFeasibleObjects(lb, comp).toList
+
+    incomps.isEmpty
   }
 
 }
