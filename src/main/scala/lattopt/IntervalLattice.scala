e18f7c73528118410e016500ad87524faf02637f
hossein.hojjat <hhojjat@gmail.com>
2021-06-26 16:27:36 +0430

    succ and pred

--- src/main/scala/lattopt/IntervalLattice.scala
+++ src/main/scala/lattopt/IntervalLattice.scala
@@ -20,6 +20,9 @@ class IntervalLattice protected (lowerLimit : BigInt, upperLimit : BigInt)
     case _ => false
   }
 
+  override def toString =
+    "[" + lowerLimit + ", " + upperLimit + "]"
+
   val bottom: (Option[BigInt], Option[BigInt]) = (Some(BigInt(1)), Some(BigInt(0)))
   val top: (Option[BigInt], Option[BigInt]) = (None,None)
 
@@ -50,6 +53,11 @@ class IntervalLattice protected (lowerLimit : BigInt, upperLimit : BigInt)
   private def canonise(o : LatticeObject) : LatticeObject =
     if (isEmpty(o)) bottom else o
 
+  private def incUpper(a : BigInt) : Option[BigInt] =
+    if (a >= upperLimit) None else Some(a + 1)
+
+  private def decLower(a : BigInt) : Option[BigInt] =
+    if (a <= lowerLimit) None else Some(a - 1)
 
   // normal partial order
   val latticeOrder: PartialOrdering[(Option[BigInt], Option[BigInt])] = new PartialOrdering[LatticeObject] {
@@ -104,11 +112,50 @@ class IntervalLattice protected (lowerLimit : BigInt, upperLimit : BigInt)
     else
       canonise((max2(x._1, y._1), min2(x._2, y._2)))
 
-  def nodeCount: BigInt = ???
+  def nodeCount: BigInt =
+      1 +                                  // empty interval
+      (upperLimit - lowerLimit + 2) +      // upper = inf
+      (((upperLimit - lowerLimit + 1) * (upperLimit - lowerLimit + 4)) / 2)
 
-  def pred(x: (Option[BigInt], Option[BigInt])): Iterator[(Option[BigInt], Option[BigInt])] = ???
+  def bigIterator(start: BigInt, end: BigInt, step: BigInt = 1) =
+    Iterator.iterate(start)(_ + step).takeWhile(_ < end)
 
-  def succ(x: (Option[BigInt], Option[BigInt])): Iterator[(Option[BigInt], Option[BigInt])] = ???
+  def succ(x: (Option[BigInt], Option[BigInt])): Iterator[LatticeObject] =
+    x match {
+      case x if (isEmpty(x)) => // successor of empty is ... [1,1], [2,2], ...
+        for (i <- (bigIterator(lowerLimit, upperLimit + 1)))
+          yield (Some(i), Some(i))
+      case (None, Some(a)) =>
+        Iterator((None, incUpper(a)))
+      case (Some(a), None) =>
+        Iterator((decLower(a), None))
+      case (x@Some(a), y@Some(b)) =>
+        Iterator((decLower(a), y), (x, incUpper(b)))
+      case _ =>
+        Iterator.empty
+    }
+
+  def pred(x: (Option[BigInt], Option[BigInt])): Iterator[LatticeObject] =
+    x match {
+      case x if (isEmpty(x)) =>
+        Iterator.empty
+      case (None, None) =>
+        Iterator((None, Some(upperLimit)), (Some(lowerLimit), None))
+      case (x@Some(a), None) =>
+        Iterator((x, Some(upperLimit))) ++
+          (if (a < upperLimit) Iterator((Some(a + 1), None))
+          else Iterator.empty)
+      case (None, x@Some(a)) =>
+        Iterator((Some(lowerLimit), x)) ++
+          (if (a > lowerLimit) Iterator((None, Some(a - 1)))
+          else Iterator.empty)
+      case (x@Some(a), y@Some(b)) =>
+        (if (a < upperLimit) Iterator(canonise((Some(a + 1), y)))
+        else Iterator.empty) ++
+          (if (b > lowerLimit) Iterator(canonise((x, Some(b - 1))))
+          else Iterator.empty)
+  }
+
 
   def objectIterator : Iterator[LatticeObject] = ???
 
